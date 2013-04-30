820367d394fdfb9b283ec84287e7f1e782779ee4
Cristobal Lama <cristoballama@gmail.com>
2013-04-29 21:08:13 -0400

    Ahora si que arregle los seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,4 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-user1 = AdminUsers.create :email => "admin@example.com", :password => "password"
+user1 = AdminUser.create :email => "admin@example.com", :password => "password"
