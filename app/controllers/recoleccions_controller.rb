ad272b93659e2dc8929586545a6d489a41d14480
Cristobal Lama <cristoballama@gmail.com>
2013-05-26 16:48:02 -0400

    Error wrap approun controller

--- app/controllers/recoleccions_controller.rb
+++ app/controllers/recoleccions_controller.rb
@@ -48,61 +48,77 @@ class RecoleccionsController < ApplicationController
     recoleccions = Array.new
     all_recoleccion_valid = true
     numero_secuencia = nil
-    
     linea_inicial = params[:linea]
     codigo_paradero_inicial=params[:paradero_inicial]
     
-    # Buscar todas las lineas para encontrar el id
-    lineas = JSON.parse(open("http://citppuc.cloudapp.net/api/lineas").read)
-    #Accion para encontra el id
-    lineas.each do |linea|
-      linea_id = linea["linea_id"] if linea["codigo_linea"] == linea_inicial
-    end
-    #fetch secuencia y encontrar la sequencia donde parte
-    secuencias = JSON.parse(open("http://citppuc.cloudapp.net/api/lineas/"+linea_id.to_s).read) if linea_id
-    #Aqui cambiar la logica para los distintos horarios
-    secuencia_paraderos = secuencias["secuencias"][0]["secuencia_paraderos"]
-
-    #------
-    # secuencia_paraderos[0] -> primer paradero de la sequencia
-    #Encuentra el numero de la sequencia a la que pertenece el paradero inicial
-    secuencia_paraderos.each do |paradero|
-      if  paradero["codigo_paradero"].casecmp(codigo_paradero_inicial) == 0
-        numero_secuencia = paradero["numero_secuencia"].to_i 
-        break
+    begin    
+      # Buscar todas las lineas para encontrar el id
+      lineas = JSON.parse(open("http://citppuc.cloudapp.net/api/lineas").read)
+      #Accion para encontra el id
+      lineas.each do |linea|
+        linea_id = linea["linea_id"] if linea["codigo_linea"] == linea_inicial
       end
-    end
-    #incializa las recolecciones
-    params[:recoleccion].each do |recoleccion|
-      #revisar cual es mas cercano
-      sqr_error = 1000
-      for i in 0..10
-        i -= secuencia_paraderos.count+1  if numero_secuencia-5+i > secuencia_paraderos.count
-        if secuencia_paraderos[numero_secuencia-5+i]
-          sqr_error_act = (recoleccion["latitude"].to_f - secuencia_paraderos[numero_secuencia-5+i]["gps_latitud"].to_f)**2 +
-                        (recoleccion["longitude"].to_f - secuencia_paraderos[numero_secuencia-5+i]["gps_longitud"].to_f)**2
-          #puts sqr_error_act
-          if (sqr_error >= sqr_error_act)
-            sqr_error = sqr_error_act
-            paradero = secuencia_paraderos[numero_secuencia-5+i]["codigo_paradero"]
-          end
+      #fetch secuencia y encontrar la sequencia donde parte
+      if linea_id
+        secuencias = JSON.parse(open("http://citppuc.cloudapp.net/api/lineas/"+linea_id.to_s).read)
+      end
+      #Aqui cambiar la logica para los distintos horarios
+      secuencia_paraderos = secuencias["secuencias"][0]["secuencia_paraderos"] if secuencias
+      # secuencia_paraderos[0] -> primer paradero de la sequencia
+      #Encuentra el numero de la sequencia a la que pertenece el paradero inicial
+      secuencia_paraderos.each do |paradero|
+        if  paradero["codigo_paradero"].casecmp(codigo_paradero_inicial) == 0
+          numero_secuencia = paradero["numero_secuencia"].to_i 
+          break
         end
       end
-      #Setear datos genericos
-      recoleccion["paradero"] = paradero 
-      recoleccion["recorrido"] = linea_inicial
-      recoleccion["patente"] = params[:patente]
-      recoleccion["puerta"] = params[:puerta]
-      recoleccion["nombre"] = params[:nombre]
-      
-      new_recoleccion = Recoleccion.new(recoleccion)
-      recoleccions << new_recoleccion
-      unless new_recoleccion.valid?
-        all_recoleccion_valid = false
-        invalid_recoleccion = recoleccion
+      #incializa las recolecciones
+      params[:recoleccion].each do |recoleccion|
+        #revisar cual es mas cercano
+        sqr_error = 1000
+        for i in 0..10
+          i -= secuencia_paraderos.count+1  if numero_secuencia-5+i > secuencia_paraderos.count
+          if secuencia_paraderos[numero_secuencia-5+i]
+            sqr_error_act = (recoleccion["latitude"].to_f - secuencia_paraderos[numero_secuencia-5+i]["gps_latitud"].to_f)**2 +
+                          (recoleccion["longitude"].to_f - secuencia_paraderos[numero_secuencia-5+i]["gps_longitud"].to_f)**2
+            #puts sqr_error_act
+            if (sqr_error >= sqr_error_act)
+              sqr_error = sqr_error_act
+              paradero = secuencia_paraderos[numero_secuencia-5+i]["codigo_paradero"]
+            end
+          end
+        end
+        #Setear datos genericos
+        recoleccion["paradero"] = paradero 
+        recoleccion["recorrido"] = linea_inicial
+        recoleccion["patente"] = params[:patente]
+        recoleccion["puerta"] = params[:puerta]
+        recoleccion["nombre"] = params[:nombre]
+        
+        new_recoleccion = Recoleccion.new(recoleccion)
+        recoleccions << new_recoleccion
+        unless new_recoleccion.valid?
+          all_recoleccion_valid = false
+          invalid_recoleccion = recoleccion
+        end
+      end
+    rescue
+      P "Error al encontrar el paradero"
+      params[:recoleccion].each do |recoleccion|
+        recoleccion["patente"] = params[:patente]
+        recoleccion["puerta"] = params[:puerta]
+        recoleccion["nombre"] = params[:nombre]
+        recoleccion["recorrido"] = params[:linea]
+        recoleccion["paradero"] = "error"+params[:paradero_inicial]
+        new_recoleccion = Recoleccion.new(recoleccion)
+        recoleccions << new_recoleccion
+        unless new_recoleccion.valid?
+          all_recoleccion_valid = false
+          invalid_recoleccion = recoleccion
+        end
       end
     end
-
+    
     respond_to do |format|
       if all_recoleccion_valid
         @recoleccions = []
